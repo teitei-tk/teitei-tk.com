7182b9969a141071266cde553bc6e2917e5b14c7
teitei-tk <teitei.tk@gmail.com>
2018-01-13 20:39:47 +0900

    add source code link

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import "furtive/css/furtive.min.css";
 import { Summary } from "./components/summary";
 import { Profile } from "./components/profile";
 import { Accounts } from "./components/accounts";
+import { Footer } from "./components/footer";
 
 class App extends React.Component {
   render() {
@@ -14,6 +15,7 @@ class App extends React.Component {
         <Summary />
         <Profile />
         <Accounts />
+        <Footer />
       </div>
     );
   }
