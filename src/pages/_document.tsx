a28507ab9a5629ae46aa55cbe47bfed60c0261c9
teitei-tk <teitei.tk@gmail.com>
2020-05-20 09:44:11 +0900

    add favicon packages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,27 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="static/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="static/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="static/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="static/favion/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
