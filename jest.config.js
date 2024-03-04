874490b771b5b16836c240ea347cfc79febee69f
Yoshikatsu Higa <teitei.tk@gmail.com>
2024-03-05 01:36:18 +0900

    update testing library and ts/jest config

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,14 @@ module.exports = {
     },
   },
   moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
     // CSSモジュールのモック
     "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     // 静的ファイルのモック
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
   },
 };
